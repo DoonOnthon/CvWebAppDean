84792a0933cb6b9551a257643b8334584a33922d
Dean <249661@cursist.rockopnh.nl>
2022-04-04 21:22:50 +0200

    Add project files.

---
+++ CvWebAppDean/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CvWebAppDean.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            DateTime birthDate = Convert.ToDateTime("30/10/2000");
+            DateTime todayDate = DateTime.Today;
+            int age = todayDate.Year - birthDate.Year;
+            if (birthDate > todayDate.AddYears(-age))
+            {
+                age--;
+            }
+            ViewBag.AgeAboutMe = age;
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult HomeProjects()
+        {
+            return View();
+        }
+    }
+}
